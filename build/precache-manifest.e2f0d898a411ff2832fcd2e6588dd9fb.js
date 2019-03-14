e787b3bbd4764161fbcfc6b8e4d2258b679e77e5
Julio <jalfaro@techlaunch.io>
2019-03-14 15:28:15 -0400

    added the new build

---
+++ build/precache-manifest.e2f0d898a411ff2832fcd2e6588dd9fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "31645461bb573cf6cc92",
+    "url": "/counter-app/static/js/main.0e3ae646.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/counter-app/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "31645461bb573cf6cc92",
+    "url": "/counter-app/static/css/main.bb57e7e3.chunk.css"
+  },
+  {
+    "revision": "a472043e15f50c6df8bd76f9b6a74758",
+    "url": "/counter-app/index.html"
+  }
+];
